d81ded8bdafb0a821a72ac407804a7859b74bf97
Alberto Villa <alberto.villa@facile.it>
2019-05-23 13:25:15 +0200

    Revert "Adapt files to polymorphic optics"
    This reverts commit 98f2ede69540cf86b18e29fa6d95d9979499f33b.

--- src/Either.ts
+++ src/Either.ts
@@ -1,11 +1,11 @@
 import { Either, right, left } from 'fp-ts/lib/Either'
-import { identity } from 'fp-ts/lib/function'
+import { fromEither, none, some } from 'fp-ts/lib/Option'
 import { Prism } from '.'
 
-const r = new Prism<Either<any, any>, any>(identity, right)
+const r = new Prism<Either<any, any>, any>(fromEither, right)
 
 export const _right = <L, A>(): Prism<Either<L, A>, A> => r
 
-const l = new Prism<Either<any, any>, any>(e => e.fold(l => right(l), a => left(a)), left)
+const l = new Prism<Either<any, any>, any>(e => e.fold(some, () => none), left)
 
 export const _left = <L, A>(): Prism<Either<L, A>, L> => l
--- src/Index/Array.ts
+++ src/Index/Array.ts
@@ -1,10 +1,7 @@
-import { left, right } from 'fp-ts/lib/Either'
 import { Index, Optional } from '../index'
 import { index, updateAt } from 'fp-ts/lib/Array'
 
 export function indexArray<A = never>(): Index<Array<A>, number, A> {
   // tslint:disable-next-line: deprecation
-  return new Index(
-    i => new Optional(s => index(i, s).fold(left(s), a => right(a)), a => s => updateAt(i, a, s).getOrElse(s))
-  )
+  return new Index(i => new Optional(s => index(i, s), a => s => updateAt(i, a, s).getOrElse(s)))
 }
