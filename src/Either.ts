98f2ede69540cf86b18e29fa6d95d9979499f33b
Alberto Villa <alberto.villa@facile.it>
2019-05-23 02:06:36 +0200

    Adapt files to polymorphic optics

--- src/Either.ts
+++ src/Either.ts
@@ -1,11 +1,11 @@
 import { Either, right, left } from 'fp-ts/lib/Either'
-import { fromEither, none, some } from 'fp-ts/lib/Option'
+import { identity } from 'fp-ts/lib/function'
 import { Prism } from '.'
 
-const r = new Prism<Either<any, any>, any>(fromEither, right)
+const r = new Prism<Either<any, any>, any>(identity, right)
 
 export const _right = <L, A>(): Prism<Either<L, A>, A> => r
 
-const l = new Prism<Either<any, any>, any>(e => e.fold(some, () => none), left)
+const l = new Prism<Either<any, any>, any>(e => e.fold(l => right(l), a => left(a)), left)
 
 export const _left = <L, A>(): Prism<Either<L, A>, L> => l
--- src/Index/NonEmptyArray.ts
+++ src/Index/NonEmptyArray.ts
@@ -1,7 +1,10 @@
+import { left, right } from 'fp-ts/lib/Either'
 import { Index, Optional } from '../index'
 import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray'
 
 export function indexNonEmptyArray<A = never>(): Index<NonEmptyArray<A>, number, A> {
   // tslint:disable-next-line: deprecation
-  return new Index(i => new Optional(s => s.index(i), a => s => s.updateAt(i, a).getOrElse(s)))
+  return new Index(
+    i => new Optional(s => s.index(i).fold(left(s), a => right(a)), a => s => s.updateAt(i, a).getOrElse(s))
+  )
 }
